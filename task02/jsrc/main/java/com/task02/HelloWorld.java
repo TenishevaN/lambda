593164226f14f5bbd8ec105f8a90c5743c94f1b9
jnIOHwta <ntgraal@gmail.com>
2024-06-01 00:52:30 +0300

    Refactor

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -46,27 +46,23 @@ public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGate
     private static final int SC_NOT_FOUND = 404;
     private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
     private final Map<String, String> responseHeaders = Map.of("Content-Type", "application/json");
-    private final Map<RouteKey, Function<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse>> routeHandlers = Map.of(
-            new RouteKey("GET", "/"), this::handleGetRoot,
-            new RouteKey("GET", "/hello"), this::handleGetHello
-    );
 
     @Override
     public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent requestEvent, Context context) {
 
-        RouteKey routeKey = new RouteKey(getMethod(requestEvent), getPath(requestEvent));
-        return routeHandlers.getOrDefault(routeKey, this::notFoundResponse).apply(requestEvent);
-    }
+        System.out.println("requestEvent.getRequestContext() " + requestEvent.getRequestContext());
 
-    private APIGatewayV2HTTPResponse handleGetRoot(APIGatewayV2HTTPEvent requestEvent) {
-        return buildResponse(SC_OK, Body.ok("Use the path /hello to get greetings message"));
-    }
+        if(requestEvent.getRequestContext() == null){
+            return buildResponse(SC_OK, Body.ok("Use the path /hello to get greetings message"));
+        }
 
-    private APIGatewayV2HTTPResponse handleGetHello(APIGatewayV2HTTPEvent requestEvent) {
-        return buildResponse(SC_OK, Body.ok("Hello from Lambda"));
-    }
+        var path = getPath(requestEvent);
+        var method = getMethod(requestEvent);
 
-    private APIGatewayV2HTTPResponse notFoundResponse(APIGatewayV2HTTPEvent requestEvent) {
+        if("/hello".equals(path)){
+            return buildResponse(SC_OK, Body.ok("Hello from Lambda"));
+        }
+
         return buildResponse(SC_NOT_FOUND, Body.error(String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s",
                 getPath(requestEvent),
                 getMethod(requestEvent)
@@ -87,24 +83,6 @@ public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGate
 
     private String getPath(APIGatewayV2HTTPEvent requestEvent) {
         return requestEvent.getRequestContext().getHttp().getPath();
-    }
-
-    private class RouteKey {
-        private String method;
-        private String path;
-
-        public String getMethod() {
-            return method;
-        }
-
-        public String getPath() {
-            return path;
-        }
-
-        public RouteKey(String method, String path) {
-            this.method = method;
-            this.path = path;
-        }
     }
 
     private static class Body {

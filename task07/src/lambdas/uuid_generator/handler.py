d353ebdd365488707ab2260629b972d2ddc2f97b
jnIOHwta <ntgraal@gmail.com>
2024-07-03 21:23:55 +0300

    Add task07

---
+++ task07/src/lambdas/uuid_generator/handler.py
@@ -0,0 +1,30 @@
+import boto3
+import uuid
+import json
+
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+from datetime import datetime
+
+_LOG = get_logger('UuidGenerator-handler')
+
+
+class UuidGenerator(AbstractLambda):
+
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket('cmtr-85e8c71a-uuid-storage')
+        
+    def handle_request(self, event, context):
+      uuids = [str(uuid.uuid4()) for _ in range(10)]
+      file_name = datetime.now().isoformat() + ".json"
+      data = {"ids": uuids}
+      self.bucket.put_object(Key=file_name, Body=json.dumps(data))
+      return 200
+    
+
+HANDLER = UuidGenerator()
+
+
+def uuid_generator(event, context):
+    producer = UuidGenerator()
+    return producer.handle_request(event, context)

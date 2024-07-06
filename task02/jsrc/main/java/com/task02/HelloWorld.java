03d86609d819027e4b541451fe4b518ffe235e0b
jnIOHwta <ntgraal@gmail.com>
2024-07-06 13:52:47 +0300

    Add task02

---
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -0,0 +1,91 @@
+package com.task02;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.Architecture;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+@LambdaHandler(lambdaName = "hello_world",
+        roleName = "hello_world-role",
+        isPublishVersion = false,
+        layers = {"sdk-layer"},
+        runtime = DeploymentRuntime.JAVA11,
+        architecture = Architecture.ARM64,
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@LambdaLayer(
+        layerName = "sdk-layer",
+        libraries = {"lib/commons-lang3-3.14.0.jar", "lib/gson-2.10.1.jar"},
+        runtime = DeploymentRuntime.JAVA11,
+        architectures = {Architecture.ARM64},
+        artifactExtension = ArtifactExtension.ZIP
+)
+@LambdaUrlConfig(
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
+)
+public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+    private static final int SC_OK = 200;
+    private static final int SC_NOT_FOUND = 400;
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private final Map<String, String> responseHeaders = Map.of("Content-Type", "application/json");
+
+    @Override
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent requestEvent, Context context) {
+
+        System.out.println("requestEvent.getRequestContext() "  + requestEvent.getRequestContext());
+
+        if (requestEvent.getRequestContext() == null) {
+            return buildResponse(SC_NOT_FOUND, Map.of("statusCode", SC_NOT_FOUND,
+                                                       "message", "Bad request syntax or unsupported method. Request path: null. HTTP method: null"));
+        }
+
+        var path = getPath(requestEvent);
+
+        System.out.println("path "  + path);
+        System.out.println("/hello.equals(path) =" + "/hello".equals(path));
+
+        if ("/hello".equals(path)) {
+            return buildResponse(SC_OK, Map.of("message", "Hello from Lambda");
+        }
+
+        return buildResponse(SC_NOT_FOUND, Map.of("message", String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s",
+                                                             path,
+                                                             getMethod(requestEvent)
+        )));
+    }
+
+    private APIGatewayV2HTTPResponse buildResponse(int statusCode, Object body) {
+        return APIGatewayV2HTTPResponse.builder()
+                .withStatusCode(statusCode)
+                .withHeaders(responseHeaders)
+                .withBody(gson.toJson(body))
+                .build();
+    }
+
+    private String getMethod(APIGatewayV2HTTPEvent requestEvent) {
+        return requestEvent.getRequestContext().getHttp().getMethod();
+    }
+
+    private String getPath(APIGatewayV2HTTPEvent requestEvent) {
+        return requestEvent.getRequestContext().getHttp().getPath();
+    }
+}

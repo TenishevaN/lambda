c1c8a489010a2b3c9d460d96edd9db94ae7e8c86
jnIOHwta <ntgraal@gmail.com>
2024-09-04 00:00:01 +0300

    add task09

---
+++ task09/jsrc/main/java/com/task09/RouteKey.java
@@ -0,0 +1,35 @@
+
+package com.task09;
+import java.util.Objects;
+
+public class RouteKey {
+    String method;
+    private String path;
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public RouteKey(String method, String path) {
+        this.method = method;
+        this.path = path;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RouteKey routeKey = (RouteKey) o;
+        return Objects.equals(method, routeKey.method) &&
+                Objects.equals(path, routeKey.path);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.method, this.path);
+    }
+}

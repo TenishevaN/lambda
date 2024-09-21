3ad1b523996fe1e4cfc32e3023d2c38ee04c7762
jnIOHwta <ntgraal@gmail.com>
2024-09-22 00:10:21 +0300

    change overlap

--- task10/jsrc/main/java/com/task10/handler/RouteNotImplementedHandler.java
+++ task10/jsrc/main/java/com/task10/handler/RouteNotImplementedHandler.java
@@ -8,6 +8,13 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.DescribeTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.ResourceNotFoundException;
+import software.amazon.awssdk.services.dynamodb.model.DescribeTableResponse;
+import software.amazon.awssdk.regions.Region;
+
+
 public class RouteNotImplementedHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     private static final Logger logger = LoggerFactory.getLogger(RouteNotImplementedHandler.class);
 
@@ -22,6 +29,14 @@ public class RouteNotImplementedHandler implements RequestHandler<APIGatewayProx
         logger.info("Headers: {}", requestEvent.getHeaders());
         logger.info("Query parameters: {}", requestEvent.getQueryStringParameters());
 
+        String tableName = System.getenv("reservations_table");
+        if (!doesTableExist(tableName)) {
+            logger.error("Table does not exist: " + tableName);
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(400)
+                    .withBody("doesn't exist");
+        }
+
 
         return new APIGatewayProxyResponseEvent()
                 .withStatusCode(501)
@@ -32,6 +47,27 @@ public class RouteNotImplementedHandler implements RequestHandler<APIGatewayProx
                                         .formatted(requestEvent.getHttpMethod(), requestEvent.getPath())
                         ).toString()
                 );
+    }
+
+    private boolean doesTableExist(String tableName) {
+        logger.info("if table exist: " + tableName);
+        try {
+            DynamoDbClient dynamoDB = DynamoDbClient.builder()
+                    .region(Region.EU_CENTRAL_1)
+                    .build();
+
+            DescribeTableResponse describeTableResponse = dynamoDB.describeTable(DescribeTableRequest.builder()
+                    .tableName(tableName)
+                    .build());
+            logger.info("DescribeTableResponse: " + describeTableResponse);
+            return describeTableResponse != null && describeTableResponse.table() != null;
+        } catch (ResourceNotFoundException e) {
+            logger.info("Table does not exist: " + tableName, e);
+            return false;
+        } catch (Exception e) {
+            logger.error("Error checking if table exists: " + e.getMessage(), e);
+            return false;
+        }
     }
 
 }
